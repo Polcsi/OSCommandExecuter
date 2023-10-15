9fa7457352db29c9eb46f84a38992d5998bab801
Polcsi <pollakbence12@gmail.com>
2023-10-15 15:15:43 +0200

    basic ui concept

---
+++ OSCommandExecuter/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using OSCommandExecuter.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCommandExecuter.MVVM.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand HistoryViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public HistoryViewModel HistoryVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        { 
+            HomeVM = new HomeViewModel();
+            HistoryVM = new HistoryViewModel();
+            SettingsVM = new SettingsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            HistoryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HistoryVM;
+            });
+
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+        }
+    }
+}
